7172d9e965909dbd44e89ddee0f7f3e39d4a3384
Daria <g.savosina.dasha@gmail.com>
2025-12-15 01:46:18 +0300

    Add files via upload

---
+++ Practice_1.java
@@ -0,0 +1,32 @@
+// Пример соблюдения принципа подстановки Лисков (LSP)
+abstract class Bird {
+    public abstract void move();
+}
+
+class Sparrow extends Bird {
+    @Override
+    public void move() {
+        System.out.println("Воробей летит");
+    }
+}
+
+class Penguin extends Bird {
+    @Override
+    public void move() {
+        System.out.println("Пингвин плывёт");
+    }
+}
+
+class BirdHandler {
+    public void makeBirdMove(Bird bird) {
+        bird.move(); // Работает для любого подкласса Bird
+    }
+}
+
+public class LiskovExample {
+    public static void main(String[] args) {
+        BirdHandler handler = new BirdHandler();
+        handler.makeBirdMove(new Sparrow());  // Воробей летит
+        handler.makeBirdMove(new Penguin());  // Пингвин плывёт
+    }
+}
